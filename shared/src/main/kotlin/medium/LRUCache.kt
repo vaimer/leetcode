56a6b38e143239cbbdc80bf932ac59d609f3b8fe
imakovchik <ivan.makovchik@infobip.com>
2024-01-26 10:09:57 +0100

    Solved the problem LRU Cache with O(1) time complexity

--- shared/src/main/kotlin/medium/LRUCache.kt
+++ shared/src/main/kotlin/medium/LRUCache.kt
@@ -1,28 +1,78 @@
 package medium
 
-class LRUCache(capacity: Int) {
-    private val capacity: Int = capacity
-    private val saveKoef = capacity / 2
-    private var currentCapacity: Int = 0
+/**
+ * Problem: 146. LRU Cache
+ * Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+ * Implement the LRUCache class:
+ * - LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+ * - int get(int key) Return the value of the key if the key exists, otherwise return -1.
+ * - void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+ *
+ * The functions get and put must each run in O(1) average time complexity.
+ *
+ * Solution:
+ * Use a doubly linked list to store the key value pairs.
+ * Use a map to store the key and the corresponding node in the linked list.
+ * When a key is accessed, remove the node from the linked list and add it to the head of the linked list.
+ * When a key is added, if the capacity is full, remove the last node from the linked list and add the new node to the head of the linked list.
+ * When a key is updated, remove the node from the linked list and add it to the head of the linked list.
+ */
+class LRUCache(private val capacity: Int) {
 
-    private val map = mutableMapOf<Int, Int>()
-    private val frequencyCount = mutableMapOf<Int, Int>()
-    private val edenMap = mutableMapOf<Int,Int>()
+    private val cache = mutableMapOf<Int, Node>()
+    private val head = Node(-1, -1)
+    private val tail = Node(-1, -1)
+
+    init {
+        head.next = tail
+        tail.prev = head
+    }
+
     fun get(key: Int): Int {
-        if(map.containsKey(key)) {
-
+        if (!cache.containsKey(key)) {
+            return -1
         }
-        return map[key] ?: edenMap[key] ?: -1
+        val node = cache[key]!!
+
+        return node.value.also {
+            remove(node)
+            addNode(node)
+        }
     }
 
     fun put(key: Int, value: Int) {
-        if(currentCapacity < capacity) {
-            map[key] = value
-            currentCapacity++
-        } else {
-            val keyToRemove = map.keys.first()
-            map.remove(keyToRemove)
-            map[key] = value
+
+        if(cache.containsKey(key)) {
+            val node = cache[key]!!
+            remove(node)
+            cache[key] = addNode(Node(key, value))
+            return
         }
+
+        if (cache.size >= capacity) {
+            val nodeToRemove = tail.prev!!
+            remove(nodeToRemove)
+            cache.remove(nodeToRemove.key)
+        }
+
+        cache[key] = addNode(Node(key, value))
     }
+
+    private fun remove(node: Node) {
+        val prev = node.prev
+        val next = node.next
+        prev?.next = next
+        next?.prev = prev
+    }
+
+    private fun addNode(node: Node): Node {
+        val next = head.next
+        head.next = node
+        node.prev = head
+        node.next = next
+        next?.prev= node
+        return node
+    }
+
+    private data class Node(val key:Int, val value:Int, var prev: Node? = null, var next: Node? = null)
 }
--- shared/src/test/kotlin/medium/LRUCacheTest.kt
+++ shared/src/test/kotlin/medium/LRUCacheTest.kt
@@ -35,7 +35,78 @@ class LRUCacheTest {
                     operation = operation,
                     cache = cache,
                     key = data[index][0],
-                    value = data[index].size.takeIf { it > 1 }?.let { data[index][1] }))
+                    value = data[index].size.takeIf { it > 1 }?.let { data[index][1] })
+            )
+        }
+    }
+
+    @Test
+    fun `test case 2`() {
+        val cache = LRUCache(2)
+
+        val operations = listOf(
+            "put", "put", "get", "put", "get", "put", "get", "get", "get"
+        )
+
+        val data = listOf(
+            listOf(1, 0),
+            listOf(2, 2),
+            listOf(1),
+            listOf(3, 3),
+            listOf(2),
+            listOf(4, 4),
+            listOf(1),
+            listOf(3),
+            listOf(4)
+        )
+
+        val expected = listOf(
+            null, null, 0, null, -1, null, -1, 3, 4
+        )
+
+        operations.forEachIndexed { index, operation ->
+            assertEquals(
+                expected[index],
+                executeCacheOperation(
+                    operation = operation,
+                    cache = cache,
+                    key = data[index][0],
+                    value = data[index].size.takeIf { it > 1 }?.let { data[index][1] })
+            )
+        }
+    }
+
+    @Test
+    fun `test case 3`() {
+        val cache = LRUCache(2)
+
+        val operations = listOf(
+            "get", "put", "get", "put", "put", "get", "get"
+        )
+
+        val data = listOf(
+            listOf(2),
+            listOf(2, 6),
+            listOf(1),
+            listOf(1, 5),
+            listOf(1, 2),
+            listOf(1),
+            listOf(2)
+        )
+
+        val expected = listOf(
+            -1, null, -1, null, null, 2, 6
+        )
+
+        operations.forEachIndexed { index, operation ->
+            assertEquals(
+                expected[index],
+                executeCacheOperation(
+                    operation = operation,
+                    cache = cache,
+                    key = data[index][0],
+                    value = data[index].size.takeIf { it > 1 }?.let { data[index][1] })
+            )
         }
     }
 
@@ -50,10 +121,12 @@ class LRUCacheTest {
             cache.put(key, value!!)
             null
         }
+
         "get" -> {
             println("cache.get(${key})")
             cache.get(key)
         }
+
         else -> {
             null
         }
