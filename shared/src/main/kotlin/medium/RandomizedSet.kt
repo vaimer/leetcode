9c1a0623214968242ec5731715676a20c54f677b
vaimer <overespada@gmail.com>
2024-04-15 10:10:27 +0200

    Solved problems:  380. Insert Delete GetRandom O(1)

---
+++ shared/src/main/kotlin/medium/RandomizedSet.kt
@@ -0,0 +1,54 @@
+package medium
+
+/**
+ * Problem: 380. Insert Delete GetRandom O(1)
+ * Design a data structure that supports all following operations in average O(1) time.
+ * 
+ * Solution:
+ * Use a map to store the value and the corresponding index in the list.
+ * Use a list to store the values.
+ * When a value is added, add it to the list and store the index in the map.
+ * When a value is removed, remove it from the list and update the index in the map.
+ * When a random value is requested, generate a random index and return the value at that index.
+ */
+class RandomizedSet() {
+
+    private val map = mutableMapOf<Int, Int>().withDefault { 0 }
+    private val set = mutableListOf<Int>()
+
+    fun insert(`val`: Int): Boolean {
+        if(map.containsKey(`val`)) {
+            return false
+        }
+
+        set.add(`val`)
+        map[`val`] = set.lastIndex
+
+        return true
+    }
+
+    fun remove(`val`: Int): Boolean {
+        if(!map.contains(`val`)) {
+            return false
+        }
+
+        val currentIndex = map.getValue(`val`)!!
+        val lastValue = set.last()
+        val lastIndex = set.lastIndex
+
+        set[currentIndex] = lastValue
+        set.removeAt(lastIndex)
+
+        map[lastValue] = currentIndex
+        map.remove(`val`)
+
+        return true
+    }
+
+    fun getRandom(): Int {
+        val randomIndex = (0 .. set.lastIndex).random()
+
+        return set[randomIndex]
+    }
+
+}
